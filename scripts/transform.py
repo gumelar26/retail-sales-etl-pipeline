0b4f476203b3ef41204e178af3e08af6228d5fae
gumelar26 <gumelarmuhamad26@gmail.com>
2025-08-14 20:55:42 +0700

    transform user dataframe

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def transform_data(products_df, transactions_df):
+def transform_data(products_df, transactions_df, users_df):
     # Dataframe Product
     products_df['currency'] = products_df['price'].str[:2]
     products_df['price'] = products_df['price'].str[3:].astype(int)
@@ -8,4 +8,7 @@ def transform_data(products_df, transactions_df):
     # Dataframe Transactions
     transactions_df['transaction_date'] = pd.to_datetime(transactions_df['transaction_date'], format='%d/%m/%Y').dt.strftime('%Y-%m-%d')
 
-    return transactions_df
+    # Dataframe Users
+    users_df['email'] = users_df['email'].replace("", None)
+
+    return
