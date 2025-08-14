f719ec8feccabd2033811b9ea36e60676f7b751e
gumelar26 <gumelarmuhamad26@gmail.com>
2025-08-14 21:13:02 +0700

    add function to load data to csv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from config.setting import PRODUCTS_PATH, USERS_PATH, TRANSACTIONS_PATH, OUTPUT_PATH
 from scripts.extract import extract_csv, extract_json
 from scripts.transform import transform_data
+from scripts.load import save_to_csv
 
 # Extract
 products_df = extract_csv(PRODUCTS_PATH)
@@ -8,5 +9,8 @@ transactions_df = extract_csv(TRANSACTIONS_PATH)
 users_df = extract_json(USERS_PATH)
 
 # Transform
-transformed_df = transform_data(products_df, transactions_df, users_df)
-print(transformed_df)
+final_df = transform_data(products_df, transactions_df, users_df)
+
+# Load
+save_to_csv(final_df, OUTPUT_PATH)
+print(f"ETL selesai! file disimpan di {OUTPUT_PATH}")
---
+++ scripts/load.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def save_to_csv(df, path):
+    df.to_csv(path, index=False)
